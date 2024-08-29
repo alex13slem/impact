9352039647aebf4ec2cdd4e5162ed6350265880f
alex13slem <a.s.scherba@yandex.by>
2024-08-29 20:54:24 +0300

    Добавил персонал

--- src/lib/data/staffs.ts
+++ src/lib/data/staffs.ts
@@ -8,107 +8,79 @@ export interface Stuff {
 export const staffs: Stuff[] = [
   {
     id: 1,
-    name: 'Александр Петров',
+    name: 'Гарбер Алексей Маркович',
     description:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, quibusdam.',
     image: 'https://www.gdpconsulting.net/wp-content/uploads/2017/09/team.jpg',
   },
   {
     id: 2,
-    name: 'Екатерина Ивановна',
+    name: 'Дениев Хамид Магомедович',
     description:
       'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
     image: 'https://www.gdpconsulting.net/wp-content/uploads/2017/09/team.jpg',
   },
   {
     id: 3,
-    name: 'Михаил Сергеевич',
+    name: 'Колташов Леонид Игоревич',
     description:
       'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.',
     image: 'https://www.gdpconsulting.net/wp-content/uploads/2017/09/team.jpg',
   },
   {
     id: 4,
-    name: 'Наталья Васильевна',
+    name: 'Косян Карен Степанович',
     description:
       'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.',
     image: 'https://www.gdpconsulting.net/wp-content/uploads/2017/09/team.jpg',
   },
   {
     id: 5,
-    name: 'Иван Алексеевич',
+    name: 'Майер Максим Артурович',
     description:
       'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia.',
     image: 'https://www.gdpconsulting.net/wp-content/uploads/2017/09/team.jpg',
   },
   {
     id: 6,
-    name: 'Ольга Николаевна',
+    name: 'Каганский Максим Евгеньевич',
     description:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, quibusdam.',
     image: 'https://www.gdpconsulting.net/wp-content/uploads/2017/09/team.jpg',
   },
   {
     id: 7,
-    name: 'Павел Михайлович',
+    name: 'Киселев Юрий Владимирович',
     description:
       'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
     image: 'https://www.gdpconsulting.net/wp-content/uploads/2017/09/team.jpg',
   },
   {
     id: 8,
-    name: 'Татьяна Ивановна',
+    name: 'Смыслов Сергей Рудольфович',
     description:
       'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.',
     image: 'https://www.gdpconsulting.net/wp-content/uploads/2017/09/team.jpg',
   },
   {
     id: 9,
-    name: 'Сергей Васильевич',
+    name: 'Лазарева Алина Викторовна',
     description:
       'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.',
     image: 'https://www.gdpconsulting.net/wp-content/uploads/2017/09/team.jpg',
   },
   {
     id: 10,
-    name: 'Марина Алексеевна',
+    name: 'Шанькова Ирина Николаевна',
     description:
       'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia.',
     image: 'https://www.gdpconsulting.net/wp-content/uploads/2017/09/team.jpg',
   },
   {
     id: 11,
-    name: 'Владимир Сергеевич',
+    name: 'Савеличев Евгений Анатольевич',
     description:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, quibusdam.',
-    image: 'https://www.gdpconsulting.net/wp-content/uploads/2017/09/team.jpg',
-  },
-  {
-    id: 12,
-    name: 'Юлия Михайловна',
-    description:
-      'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    image: 'https://www.gdpconsulting.net/wp-content/uploads/2017/09/team.jpg',
-  },
-  {
-    id: 13,
-    name: 'Дмитрий Иванович',
-    description:
-      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.',
-    image: 'https://www.gdpconsulting.net/wp-content/uploads/2017/09/team.jpg',
-  },
-  {
-    id: 14,
-    name: 'Анастасия Васильевна',
-    description:
-      'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.',
-    image: 'https://www.gdpconsulting.net/wp-content/uploads/2017/09/team.jpg',
-  },
-  {
-    id: 15,
-    name: 'Кирилл Алексеевич',
-    description:
-      'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia.',
     image: 'https://www.gdpconsulting.net/wp-content/uploads/2017/09/team.jpg',
   },
 ];
--- src/lib/data/staffsToPositions.ts
+++ src/lib/data/staffsToPositions.ts
@@ -8,76 +8,56 @@ const staffToPositions: StuffsToPositions[] = [
   {
     id: 1,
     stuffId: 1,
-    positionId: 1,
+    positionId: 4,
   },
   {
     id: 2,
     stuffId: 2,
-    positionId: 1,
+    positionId: 4,
   },
   {
     id: 3,
     stuffId: 3,
-    positionId: 1,
+    positionId: 4,
   },
   {
     id: 4,
     stuffId: 4,
-    positionId: 1,
+    positionId: 4,
   },
   {
     id: 5,
     stuffId: 5,
-    positionId: 1,
+    positionId: 4,
   },
   {
     id: 5,
     stuffId: 6,
-    positionId: 2,
+    positionId: 1,
   },
   {
     id: 6,
     stuffId: 7,
-    positionId: 3,
+    positionId: 1,
   },
   {
     id: 7,
     stuffId: 8,
-    positionId: 4,
+    positionId: 1,
   },
   {
     id: 8,
     stuffId: 9,
-    positionId: 4,
+    positionId: 2,
   },
   {
     id: 9,
     stuffId: 10,
-    positionId: 4,
+    positionId: 3,
   },
   {
     id: 10,
     stuffId: 11,
-    positionId: 4,
-  },
-  {
-    id: 11,
-    stuffId: 12,
-    positionId: 5,
-  },
-  {
-    id: 12,
-    stuffId: 13,
-    positionId: 5,
-  },
-  {
-    id: 13,
-    stuffId: 14,
-    positionId: 5,
-  },
-  {
-    id: 14,
-    stuffId: 15,
     positionId: 5,
   },
 ];
