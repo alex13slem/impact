ceff9c91bb7d3b078e3abb51ab6f4f20a333f0ce
alex13slem <a.s.scherba@yandex.by>
2024-09-09 00:17:44 +0300

    test host

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,11 +13,11 @@ export default defineConfig({
     mode: 'standalone',
   }),
   server: {
-    port: 3000,
+    port: 4321,
     host: true,
   },
   preview: {
-    port: 3000,
+    port: 4321,
     host: true,
   },
 });
