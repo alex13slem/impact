b089bdf12d933e497a750854ab7d01ecdf9457d8
alex13slem <a.s.scherba@yandex.by>
2024-09-08 20:06:31 +0300

    docploy config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,27 @@
-import netlify from '@astrojs/netlify';
 import svelte from '@astrojs/svelte';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
+
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
   output: 'hybrid',
-  adapter: netlify(),
+  adapter: node({
+    mode: 'standalone',
+  }),
+  server: {
+    port: 3000,
+    host: true,
+  },
+  vite: {
+    ssr: {
+      noExternal: ['path-to-regexp'],
+    },
+    preview: {
+      port: 3000,
+      host: true,
+    },
+  },
 });
