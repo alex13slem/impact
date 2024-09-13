836a0d5d43497d2bf4daac537136025bca2fcef8
alex13slem <a.s.scherba@yandex.by>
2024-09-13 14:34:05 +0300

    fix counter

--- src/lib/plugins/hiddenBlocks.ts
+++ src/lib/plugins/hiddenBlocks.ts
@@ -1,8 +1,11 @@
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+
 const hiddenBlocks = document.querySelectorAll('.hidden-block');
 const observer = new IntersectionObserver(entries => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
       entry.target.classList.add('hidden-block--visible');
+      ScrollTrigger.refresh();
     }
   });
 });
