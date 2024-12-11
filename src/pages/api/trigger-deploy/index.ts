5ea97a4a0a353cdd394a5288be06aa30b41fe361
alex13slem <o.s.shcherba@gmail.com>
2024-12-11 21:34:30 +0300

    fix trigger-deploy

--- src/pages/api/trigger-deploy/index.ts
+++ src/pages/api/trigger-deploy/index.ts
@@ -3,47 +3,23 @@ import axios from "axios";
 import "dotenv/config";
 
 export const prerender = false;
-const ALLOWED_ORIGIN = "https://api.bnbhost.ru";
 
-export const POST: APIRoute = async ({ request }) => {
-  const origin = request.headers.get("origin");
-
-  if (origin !== ALLOWED_ORIGIN) {
-    return new Response(null, { status: 403 });
-  }
-
-  const headers = new Headers({
-    "Access-Control-Allow-Origin": origin,
-    "Access-Control-Allow-Methods": "POST, OPTIONS",
-    "Access-Control-Allow-Headers": "Content-Type",
-  });
-
+export const GET: APIRoute = async () => {
+  const data = { applicationId: process.env.DOKPLOY_APP_ID };
   try {
-    const response = await axios.get("/api/deployment.all", {
-      baseURL: process.env.DOKPLOY_URL,
-      headers: {
-        Authorization: "Bearer " + process.env.DOKPLOY_AUTH_TOKEN,
-      },
-      params: { applicationId: process.env.DOKPLOY_APP_ID },
-    });
-    return new Response(JSON.stringify(response.data), {
-      status: 200,
-      headers,
-    });
+    const response = await axios.post(
+      "/api/application.deploy",
+      JSON.stringify(data),
+      {
+        baseURL: process.env.DOKPLOY_URL,
+        headers: {
+          Authorization: "Bearer " + process.env.DOKPLOY_AUTH_TOKEN,
+        },
+      }
+    );
+    return new Response(JSON.stringify(response.data), { status: 200 });
   } catch (error) {
     console.error(error);
-    return new Response(null, { status: 500, headers });
+    return new Response(JSON.stringify(error), { status: 500 });
   }
-};
-
-export const OPTIONS: APIRoute = async ({ request }) => {
-  const origin = request.headers.get("origin");
-
-  const headers = new Headers({
-    "Access-Control-Allow-Origin": origin === ALLOWED_ORIGIN ? origin : "",
-    "Access-Control-Allow-Methods": "POST, OPTIONS",
-    "Access-Control-Allow-Headers": "Content-Type",
-  });
-
-  return new Response(null, { status: 204, headers });
 };
