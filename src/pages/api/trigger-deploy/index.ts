4e9d5348c3e3dd208912c890ee1db2177712cef2
alex13slem <o.s.shcherba@gmail.com>
2024-12-11 19:31:11 +0300

    fix trigger-deploy

--- src/pages/api/trigger-deploy/index.ts
+++ src/pages/api/trigger-deploy/index.ts
@@ -1,23 +1,17 @@
-import type { APIRoute } from 'astro';
-import axios from 'axios';
-import 'dotenv/config';
+import type { APIRoute } from "astro";
+import axios from "axios";
+import "dotenv/config";
 
 export const prerender = false;
 
 export const POST: APIRoute = async () => {
-  const data = { applicationId: process.env.DOKPLOY_APP_ID };
-  const response = await axios.post(
-    '/api/application.deploy',
-    JSON.stringify(data),
-    {
-      baseURL: process.env.DOKPLOY_URL,
-      headers: {
-        accept: 'application/json',
-        'Content-Type': 'application/json',
-        Authorization: 'Bearer ' + process.env.DOKPLOY_AUTH_TOKEN,
-      },
+  const response = await axios.get("/api/deployment.all", {
+    baseURL: process.env.DOKPLOY_URL,
+    headers: {
+      Authorization: "Bearer " + process.env.DOKPLOY_AUTH_TOKEN,
     },
-  );
+    params: { applicationId: process.env.DOKPLOY_APP_ID },
+  });
 
   return new Response(JSON.stringify(response.data), { status: 200 });
 };
